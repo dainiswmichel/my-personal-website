40e86ad0137db68fe392d652e4b8aeaa6a9c0c95
Dainis W. Michel <dainis@dainis.net>
2024-02-12 02:23:45 +0100

    preliminary site 02

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Mini-Bio Site";
+h2.style.textAlign = "center";
+h2.style.paddingTop = "37px";
 
 document.querySelector("body").appendChild(h2);
